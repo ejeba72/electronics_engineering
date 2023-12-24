56a21b8b655ee2020defee74eb790c17a1c65088
Emmanuel Eni <eejeba@gmail.com>
2023-12-24 23:19:36 +0100

    Wrote code to control servo.

---
+++ arduino/code/1.practice/12.joystick_servo_control/12.joystick_servo_control.ino
@@ -0,0 +1,8 @@
+// Joystick-controlled Servo
+// 2023.dec24 sun 2316mf
+/*
+Steps:
+- write code for joystick.
+- use serial monitor to examine joystick code
+- then develop the code to include the servo
+*/
