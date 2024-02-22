1e805f1a373f85ca880ea32318c410b719edba1b
Emmanuel Eni <eejeba@gmail.com>
2024-02-22 11:03:58 +0100

    latest commit

---
+++ 03.avr_atmega328/code/1.practice/armlite_version/09.blink_led/09.blink_led.s
@@ -0,0 +1,11 @@
+BLINKING LED, FREESTYLE APPROACH
+204feb22 thu 1042mf
+
+; choice of pin: d2 = pd2 (ie digital pin2 on arduino)
+
+; timer would consist of three loops: 1. outerLoop, 2. innerLoop, 3. innerInnerLoop
+
+; DDRD = 0x2a
+; PORTD = 0x2b
+
+; to-do: flash avr code into m328p device. observe the behaviour of the led. Try to write code in armlite that replicate such behaviour. Also, find the clock cycle of armlite LMC (Little Man Computer).
