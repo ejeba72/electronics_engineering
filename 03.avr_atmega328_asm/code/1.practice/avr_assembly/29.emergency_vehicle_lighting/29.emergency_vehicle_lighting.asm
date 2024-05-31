6570872378fde2907e84a758ed9fb57f149833ca
Emmanuel Eni <eejeba@gmail.com>
2024-05-31 21:43:42 +0100

    Commencement of project: Emergency Vehicle Lighting.

---
+++ 03.avr_atmega328_asm/code/1.practice/avr_assembly/29.emergency_vehicle_lighting/29.emergency_vehicle_lighting.asm
@@ -0,0 +1,3 @@
+; 2139mf fri 31may2024
+
+; This program is for blinking Emergency Vehicle Lighting. The will be three toggle switches to select colour combination of the light. And a fourth toggle switch to off or on the light.
