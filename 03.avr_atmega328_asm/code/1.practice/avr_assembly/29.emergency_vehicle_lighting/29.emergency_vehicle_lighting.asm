21de112f6d37bc00174679644d601b7f1d680ee6
Emmanuel Eni <eejeba@gmail.com>
2024-06-02 14:28:39 +0100

    Wrote delay logic for toggle button.

--- 03.avr_atmega328_asm/code/1.practice/avr_assembly/29.emergency_vehicle_lighting/29.emergency_vehicle_lighting.asm
+++ 03.avr_atmega328_asm/code/1.practice/avr_assembly/29.emergency_vehicle_lighting/29.emergency_vehicle_lighting.asm
@@ -1,3 +1,31 @@
-; 2139mf fri 31may2024
+; 1239mf fri 31may2024
 
 ; This program is for blinking Emergency Vehicle Lighting. The will be three toggle switches to select colour combination of the light. And a fourth toggle switch to off or on the light.
+
+; To-do:
+; - Write delay logic for toggle button.
+; - Write toggle button logic.
+; - Write logic for long delay and short delay.
+; - Write logic to switch on each of the three emergency lighting options Green & Blue, Green & Red, Blue & Red.
+; - Write logic to turn emergency lights off.
+
+.device atmega328p
+
+btnDelay:
+  push r18, r19, r20
+  ldi r18, 3
+outer:
+  subi r18, 1
+  ldi r19, 255
+inner:
+  subi r19, 1
+  ldi r20, 255
+innerInner:
+  cpi r20, 0
+  brne innerInner
+  cpi r19, 0
+  brne inner
+  cpi r18, 0
+  brne outer
+  pop r18, r19, r20
+  ret
