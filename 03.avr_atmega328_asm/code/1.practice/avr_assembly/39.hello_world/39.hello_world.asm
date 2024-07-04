23775a09969aac0126acc48d90d34df97f19798c
Emmanuel Eni <eejeba@gmail.com>
2024-07-04 19:10:49 +0100

    Commit progress.

--- 03.avr_atmega328_asm/code/1.practice/avr_assembly/39.hello_world/39.hello_world.asm
+++ 03.avr_atmega328_asm/code/1.practice/avr_assembly/39.hello_world/39.hello_world.asm
@@ -1,26 +1,21 @@
 ; 1536mf wed jul3,2024
 ; 'hello, world' Program
 
-; UCSR0A: USART Control and Status Reg A
-; UCSR0B: USART Control and Status Reg B
-; UCSR0C: USART Control and Status Reg C
-; UBRR0L: USART Baud Rate Reg, Low
+; UCSR0A: USART Control and Status Reg A (0x00. However, check bit5.)
+; UCSR0B: USART Control and Status Reg B (0x18)
+; UCSR0C: USART Control and Status Reg C (0x06. Async, Parity disabled, 1 stop bit, 8-bit character.)
+; UBRR0L: USART Baud Rate Reg, Low (For baudrate = 9600, UBRR = 103 = 0b01100111 = 0x67)
 ; UBRR0H: USART Baud Rate Reg, High
 ; UDR0: USART Data Reg
 
 .device atmega328p
 
-.equ ddrb = 4  ; 0x04 = 4.
-.equ portb = 5  ; 0x05 = 5.
-
-; Test Program: Blink LED
-  sbi ddrb, 5
-loop:
-  sbi portb, 5
-  rcall delay
-  cbi portb, 5
-  rcall delay
-  rjmp loop
+.equ UCSR0A = 0xc0
+.equ UCSR0B = 0xc1
+.equ UCSR0C = 0xc2
+.equ UBRR0L = 0xc4
+.equ UBRR0H = 0xc5
+.equ UBR0 = 0xc6
 
 delay:
   push r18, r19, r20
