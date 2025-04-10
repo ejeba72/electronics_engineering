6718743adb69ff9c7ce80fb3dc93a27926dee056
Emmanuel Eni <eejeba@gmail.com>
2025-04-10 21:42:50 +0200

    Modify 35.led_chaser.c

--- 04.atmega_328_in_c/code/35.led_chaser/35.led_chaser.c
+++ 04.atmega_328_in_c/code/35.led_chaser/35.led_chaser.c
@@ -1,3 +1,27 @@
 // 0231am wed2apr2025
 // LED Chaser Program
 // with respect to delay, scale down by a factor of 10. E.g. 100ms will be 10ms.
+
+#define F_CPU 8000000UL
+
+#include<avr/io.h>
+#include<util/delay.h>
+
+#define LED1 0
+#define LED2 1
+#define LED3 2
+#define LED4 3
+#define LED5 4
+#define LED6 5
+
+int main(void) {
+  // initializations
+  DDRB = 0x3F;
+  // event loop
+  while (1) {
+    PORTB = 0x07;
+    _delay_ms(20);
+    PORTB = 0X38;
+    _delay_ms(20);
+  }
+}
